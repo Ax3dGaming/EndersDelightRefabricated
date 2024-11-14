56d3b4f21de853bab3e3da5c56f40b40c655be5c
Ax3dGaming <ax3dgaming@gmail.com>
2024-11-14 21:07:24 +0100

    Loot tables

--- src/main/java/com/axedgaming/EndersDelight.java
+++ src/main/java/com/axedgaming/EndersDelight.java
@@ -16,9 +16,6 @@ public class EndersDelight implements ModInitializer {
 
     @Override
     public void onInitialize() {
-        // This code runs as soon as Minecraft is in a mod-load-ready state.
-        // However, some things (like resources) may still be uninitialized.
-        // Proceed with mild caution.
 
         EDBlocks.BLOCKS.register();
         EDBlockEntityTypes.TILES.register();
@@ -27,7 +24,5 @@ public class EndersDelight implements ModInitializer {
         EDEffects.init();
         EDPlayer.init();
         EDDamageSource.WATERED.registry();
-
-        LOGGER.info("Hello Fabric world!");
     }
 }
